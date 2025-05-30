458dbee46749f1faa534e10ced69d77a3df591dc
Özgür Baserdem <ozguer.baserdem@sj.se>
2025-05-30 23:49:00 +0200

    Change next config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // Required for static export
-  },
-  output: isProd ? "export" : undefined,
-  // This ensures the app works with GitHub Pages
-  trailingSlash: true,
+  // THESE ARE NOT NEEDED IN VERCEL
+  // images: {
+  //   unoptimized: true, // Required for static export
+  // },
+  // output: isProd ? "export" : undefined,
+  // // This ensures the app works with GitHub Pages
+  // trailingSlash: true,
 
   // Exclude development extensions in production
   webpack: (config, { dev, isServer }) => {
